3c98ddf587332f176a1cea687e65fd5c569f76b1
AndrewGalynin <galynin69@gmail.com>
2023-10-20 15:44:54 +0300

    asp.net 7 lab
    asp.net 7 lab

---
+++ Lab 7/Controllers/FileController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab_7.Models;
+using System.IO;
+
+namespace Lab_7.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFile(FileViewModel model)
+        {
+            var content = $"Ім'я: {model.FirstName}\nПрізвище: {model.LastName}";
+            var filePath = $"{model.FileName}.txt";
+
+            System.IO.File.WriteAllText(filePath, content);
+
+            return File(System.IO.File.ReadAllBytes(filePath), "text/plain", filePath);
+        }
+    }
+}
---
+++ Lab 7/Models/FileViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_7.Models
+{
+    public class FileViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FileName { get; set; }
+
+    }
+}
